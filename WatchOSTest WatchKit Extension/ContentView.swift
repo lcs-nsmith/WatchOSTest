c128e80e2e5386fa5cafa1464c7ba38f33cead31
Nathan Smith <nsmith@lcs.on.ca>
2022-05-05 12:02:50 -0400

    having simulator problems, but it should work

--- WatchOSTest WatchKit Extension/ContentView.swift
+++ WatchOSTest WatchKit Extension/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        Image("leon")
+            .resizable()
+            .clipped()
+            .frame(width: 200, height: 200, alignment: .center)
     }
 }
 
